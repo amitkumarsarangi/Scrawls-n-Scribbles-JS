4a482dacb5dd55557f731f5d441f4dc55796816c
Amit Kumar Sarangi <amitkumarsarangi@users.noreply.github.com>
2018-04-08 09:56:02 +0200

    Add first project - A Preload Animation

---
+++ A-Preloader-Animation/main.js
@@ -0,0 +1,36 @@
+var preloader = document.getElementById("preloader");
+
+window.onload = function() {
+	var allTags = document.querySelectorAll("*");
+	for(var i=0; i < allTags.length; i++) {
+	    allTags[i].style.margin = '0';
+	    allTags[i].style.padding = '0';
+	    allTags[i].style.border = '0';
+	}
+
+	preloader.style.margin = '0 auto';
+	preloader.style.marginTop = '40vh';
+	preloader.style.width = '2px';
+	preloader.style.height = '70px';
+
+	preloader.innerHTML = '<div id="animation"></div>';
+
+	var animation = document.getElementById('animation');
+
+	animation.style.width = '100%';
+	animation.style.backgroundColor = '#555';
+	animation.style.boxShadow = '0 0 15px 0 #000';
+
+	var heightVar = 0;
+	var rotationVar = 0;
+	setInterval(function(){
+		heightVar += 10;
+		animation.style.height = heightVar + '%';
+
+		if(heightVar===100) {
+			heightVar = 0;
+			rotationVar += 40;
+			preloader.style.transform = 'rotate(' + rotationVar + 'deg)';
+		}
+	}, 1);
+}
